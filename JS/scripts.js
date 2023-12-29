3864704823b85fbcb1580ca987da3f1f07991405
david <davidlucasloureiro@gmail.com>
2023-12-29 21:09:48 +0000

    .

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,31 +1,99 @@
-let name = document.querySelector("#name");
-let amount = document.querySelector("#amount");
-let donate = document.querySelector("#donate");
-let message = "";
-let monthly = document.querySelector("#monthly");
+window.onload = function () {
+    init();
+};
 
-donate.addEventListner('click', function(){
-    donate();
+var form = document.querySelector('form');
+// Evento para que o submit não submeta qualquer dados
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+});
+
+let Name = document.querySelector('#Name');
+let Amount = document.querySelector('#Amount');
+let btnDonate = document.querySelector('#btnDonate');
+let Monthly = document.querySelector('#Monthly');
+let msg = document.querySelector('#msg');
+let situacao = document.querySelector('#situacao');
+let Message = "";
+
+function init() {
+    Name.value = '';
+    Amount.value = '0';
+    Message='';
+}
+
+btnDonate.addEventListener('click', function () {
+ 
+    checkForm();
 })
 
-function donate(){
-    let meals = Math.floor(amount.value/1.5);
+function checkName(fld) {
+    var letters = /^[A-zÀ-ú]+$/;
+    if (fld.value.trim().match(letters))
+        return true;
+    return false;
+}
+
+function checkForm() {
+    if (form.checkValidity()) {
+        if(!checkName(Name)){
+            Name.setCustomValidity('Nome Inválido!');
+            return;
+        }
+        debugger
+       let a = parseFloat(Amount.value);
+       if(a<=0){
+        setCustomValidity('Montante deve ser maior que zero');
+            return;
+       }
+
+       donate(a);
+    }
+    else {
+        form.querySelectorAll(':invalid')[0].focus();
+    }
+}
+
+function donate(m){
+    console.log("m: "+m);
+    let meals = Math.floor(m/1.5);
+    console.log("meals: "+meals);
     let daily_meals = Math.floor(meals/2);
-    if(monthly.checked==false)
-        if(amount.value>=40){
-            message = "O seu donativo permitirá fornecer aproximadamente "+ meals +" refeições";
+    console.log("dailymeals: "+daily_meals);
+    let monthly_meals = Math.floor(daily_meals/30);
+    console.log("monthlymeals: "+monthly_meals);
+    if(Monthly.checked == true)
+        if(m>=40){
+            if(meals==1){
+                Message = "O seu donativo permitirá fornecer aproximadamente "+ meals +" refeição";
+            } else{
+                Message = "O seu donativo permitirá fornecer aproximadamente "+ meals +" refeições";
+            }
         }
         else {
-            message = "O seu donativo permitirá alimentar diariamente "+ daily_meals +" pessoas";
+            if(daily_meals==1){
+                Message = "O seu donativo permitirá alimentar diariamente "+ daily_meals +" pessoa";
+            }else{
+                Message = "O seu donativo permitirá alimentar diariamente "+ daily_meals +" pessoas";
+            }
+            
         }
     else{
-        if(amount.value>=90){
-            message = "O seu donativo permitirá alimentar "+ Math.floor(daily_meals/30) +" pessoas durante este mês"
+        if(Amount.value>=90){
+            if(monthly_meals==1){
+                Message = "O seu donativo permitirá alimentar "+ monthly_meals +" pessoa durante este mês";
+            }else{
+                Message = "O seu donativo permitirá alimentar "+ monthly_meals +" pessoas durante este mês";
+            }
         }
         else{
-            message = "O seu donativo permitirá alimentar uma pessoa durante "+ daily_meals +" dias";
+            if(daily_meals==1){
+                Message = "O seu donativo permitirá alimentar uma pessoa durante "+ daily_meals +" dia";
+            }else{
+                Message = "O seu donativo permitirá alimentar uma pessoa durante "+ daily_meals +" dias";
+            }
         }
     }
-
-    return message;
+    console.log(Message, Monthly.checked);
+    msg.textContent=Message;
 }
